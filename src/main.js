968e4467e4534953ebb9a7893b6b274e2c61ddb9
ElizabethTorre <etr604@gmail.com>
2019-09-17 17:12:25 -0500

    Creandoo y testeando función para reconocer si la ruta es file o directory close #8 close #9

--- src/main.js
+++ src/main.js
@@ -1,23 +1,36 @@
-// const fs = require('fs');
+const fs = require('fs');
 const path = require('path');
 // process.cwd() => ruta actual
 // ../markdown/first.md
 
 // Leer la ruta ingresada y convertirla en absoluta si no lo es
 
-const verifyPathAbs = () => {
+const verifyPathAbs = (router) => {
   if (path.isAbsolute(router)) {
-    return route;
+    return router;
   }
   return path.resolve(router);
   // const ruta = path.isAbsolute(router);
-  // console.log(ruta);
+  // console.log(router);
 };
+// verifyPathAbs('../src/main.js');
 
 // Retorna un buleano si cumple con la extensión md
 const extensionmd = (router) => path.extname(router) === '.md';
 
-module.exports = {
+// Retorna 'Is file' o 'Is Directory' si la ruta corresponde
+// https://www.technicalkeeda.com/nodejs-tutorials/how-to-check-if-path-is-file-or-directory-using-nodejs
+
+const isFileOrDirectory = (router) => {
+  if(fs.statSync(router).isFile()) {
+    return 'Is file';
+  }
+  return 'Is Directory';
+};
+
+
+export {
   verifyPathAbs,
   extensionmd,
+  isFileOrDirectory,
 };
--- test/index.spec.js
+++ test/index.spec.js
@@ -1,21 +1,31 @@
 // importamos la funcion que vamos a testear
-import { verifyPathAbs, extensionmd } from '../src/main.js';
 
-const relativePath = '..test/index.spec.js';
-const absolutaPath = process.cwd();
+const path = require('path');
+
+import { verifyPathAbs, extensionmd, isFileOrDirectory } from '../src/main.js';
+
+// const relativePath = 'test\\index.spec.js';
+// const absolutaPath = 'C:\\Users\\Laboratoria\\Desktop\\PROYECTOS_LABORATORIA\\TRACK FED\\LIM010-fe-md-links\\test\\index.spec.js';
 const nonPathMd = 'markdown/file.js';
 
 describe('verifyPathAbs', () => {
   it('Si la ruta es absoluta, debería devolverla', () => {
-    expect(verifyPathAbs(absolutaPath)).toBe(absolutaPath);
+    expect(verifyPathAbs(path.join(process.cwd(),'test'))).toBe(path.join(process.cwd(),'test'));
   });
   it('Si no es absoluta, debería convertirla', () => {
-    expect(verifyPathAbs(relativePath)).toBe(absolutaPath);
+    expect(verifyPathAbs('test')).toBe(path.join(process.cwd(),'test'));
   });
 });
 
 describe('extensionmd', () => {
   it('Si la ruta es de extensión .md devolverá true, de los contrario false', () => {
-    expect(extensionmd(nonPathMd)).toBe(false);
+    expect(extensionmd(path.join(process.cwd(),'markdown\file.js'))).toBe(false);
   });
 });
+
+describe('isFileOrDirectory', () => {
+  it('Si la ruta es file devolverá -Is file- de los contrario -Is Directory-', () => {
+    expect(isFileOrDirectory(path.join(process.cwd(),'markdown'))).toBe('Is Directory');
+  });
+});
+// console.log(path.join(process.cwd(),'hola'));
