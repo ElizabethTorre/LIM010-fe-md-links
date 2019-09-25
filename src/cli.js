e5fad94027b7b9db89fd9e304b3705d64bd905b0
ElizabethTorre <etr604@gmail.com>
2019-09-25 18:53:15 -0500

    Probando ejecutar desde terminal el ingreso de rutas

--- src/cli.js
+++ src/cli.js
@@ -1,13 +1,20 @@
 #!/usr/bin/env node
-const [, , ... args] = process.argv
-console.log(`Hello ${args}`);
+// const [, , ... args] = process.argv
+// console.log(`Hello ${args}`);
 
-// import { mdLinks } from './mdLinks.js';
-// import { optionStats, optionStatsValidate} from './options.js';
-// const path = process.argv[2];
-// const firstOption = process.argv[3];
-// const secondOption = process.argv[4];
+import { mdLinks } from './mdLinks.js';
+import { optionStats, optionStatsValidate} from './options.js';
+const fs = require('fs');
+const path = process.argv[2];
+const firstOption = process.argv[3];
+const secondOption = process.argv[4];
 
-// const cli = () => {
-//     if()
-// }
+const cli = (path) => {
+    if(path === undefined) {
+        console.log('Ingresar ruta');
+    } else if (fs.existsSync(path)) {
+        console.log('Correcto');
+    } 
+    console.log('ruta no existente');
+}
+cli(path);
