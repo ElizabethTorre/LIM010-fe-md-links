a5e8ca120df8176b4780e89a51e2880b79892c33
ElizabethTorre <etr604@gmail.com>
2019-09-26 00:42:19 -0500

    Añadiendp condiciones a cli.js para las respuestas

--- src/cli.js
+++ src/cli.js
@@ -9,12 +9,25 @@ const path = process.argv[2];
 const firstOption = process.argv[3];
 const secondOption = process.argv[4];
 
-const cli = (path) => {
-    if(path === undefined) {
-        console.log('Ingresar ruta');
-    } else if (fs.existsSync(path)) {
-        console.log('Correcto');
-    } 
-    console.log('ruta no existente');
+const cli = (path, validate, stats) => {
+
+  if(path === undefined && validate === undefined && stats === undefined) {
+    console.log('Ingresar ruta');
+  } else if (path !== undefined && validate === undefined && stats === undefined) {
+    mdLinks(path, {validate:false}).then(rsp => console.log(rsp));
+  } else if (path !== undefined && validate === '--validate' && stats === undefined) {
+    mdLinks(path, {validate:true}).then(rsp => console.log(rsp));
+  } else if (path !== undefined && validate === '--stats' && stats === undefined) {
+    mdLinks(path, {validate:false}).then(rsp => console.log(optionStats(rsp)));
+  } else if (path !== undefined && validate === '--validate' && stats === '--stats') {
+    // mdLinks(path, {validate:true}).then(rsp => console.log(optionStatsValidate(rsp)));
+  }
+    // if(path === undefined) {
+    //     console.log('Ingresar ruta');
+    // } else if (fs.existsSync(path)) {
+    //     console.log('Correcto');
+    // } else {
+    // console.log('ruta no existente');
+    // }
 }
-cli(path);
+cli(path, firstOption, secondOption);
